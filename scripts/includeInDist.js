0c7b160ddd9df2e6e0bfa7f43a80772cb60db093
eldorado230 <98624905+eldorado230@users.noreply.github.com>
2023-04-09 19:20:22 +0100

    Update includeInDist.js

--- scripts/includeInDist.js
+++ scripts/includeInDist.js
@@ -1,21 +1,26 @@
 import path from 'path';
 import log from 'electron-log';
+import shell from 'shelljs';
 
-const shell = require('shelljs');
+log.info('Running includeInDist script to copy necessary files to dist folder for packaging.');
 
-// as the dist folder is not synced on github, we copy files into it before packaging
-log.info('running includeInDist script to copy some files into the dist folder for later packaging');
-
-// set variables
+// Set paths to relevant directories
 const moviePrintDir = shell.pwd().stdout;
-const distDir = path.resolve(moviePrintDir, 'app/dist/');
-const resourcesDir = path.resolve(moviePrintDir, 'resources/');
-// log.debug(moviePrintDir);
-// log.debug(distDir);
-// log.debug(resourcesDir);
+const distDir = path.resolve(moviePrintDir, 'app', 'dist');
+const resourcesDir = path.resolve(moviePrintDir, 'resources');
 
-// copy files
-shell.set('-v'); // verbose
-shell.mkdir('-p', distDir); // create folder if it does not exist yet
-shell.cp('-nf', path.resolve(resourcesDir, 'font/Franchise-Bold.woff'), distDir);
-shell.cp('-nfr', path.resolve(resourcesDir, 'weights/'), distDir);
+// Ensure that dist directory exists
+shell.mkdir('-p', distDir);
+
+// Copy necessary files to dist directory
+const filesToCopy = [
+  'font/Franchise-Bold.woff',
+  'weights/',
+];
+filesToCopy.forEach(file => {
+  const source = path.resolve(resourcesDir, file);
+  shell.cp('-nfr', source, distDir);
+});
+
+// Set shell verbosity to "verbose"
+shell.config.verbose = true;
